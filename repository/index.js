08e549c8dacf194768655fc63a4e7514af3f084c
Roel <roel@wheelhouse.be>
2019-09-25 15:31:45 +0200

    KAS-795 Added document title to the document group query

--- repository/index.js
+++ repository/index.js
@@ -1,8 +1,9 @@
 import { query } from 'mu';
+
 const targetGraph = 'http://mu.semte.ch/graphs/organizations/kanselarij';
 
 const getAllAgendaItemsFromAgendaWithDocuments = async (agendaId) => {
-  const queryString = `
+    const queryString = `
     PREFIX vo-org: <https://data.vlaanderen.be/ns/organisatie#>
     PREFIX besluit: <http://data.vlaanderen.be/ns/besluit#>
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
@@ -16,45 +17,46 @@ const getAllAgendaItemsFromAgendaWithDocuments = async (agendaId) => {
     PREFIX dbpedia: <http://dbpedia.org/ontology/>
     PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
     
-    SELECT (MAX(?versionNumber) as ?maxVersionNumber) ?documentVersionId ?numberVR ?extension ?download ?agendaitemPrio ?agendaitem_id ?documentVersionName  WHERE { 
+    SELECT (MAX(?versionNumber) as ?maxVersionNumber) ?documentVersionId ?numberVR ?extension ?download ?agendaitemPrio ?agendaitem_id ?documentVersionName ?documentTitle  WHERE { 
        GRAPH <${targetGraph}>
        {
          ?agenda a besluitvorming:Agenda ;
-                    mu:uuid "${agendaId}" .
-         ?agenda   ext:agendaNaam ?agendaName .
-         ?agenda   dct:hasPart ?agendaitem .
+                   mu:uuid "${agendaId}" ;
+                   ext:agendaNaam ?agendaName ;
+                   dct:hasPart ?agendaitem .
          ?agendaitem mu:uuid ?agendaitem_id .
-         OPTIONAL   { ?agendaitem ext:prioriteit ?agendaitemPrio . }
+         OPTIONAL { ?agendaitem ext:prioriteit ?agendaitemPrio . }
          OPTIONAL { 
 					 ?agendaitem ext:bevatAgendapuntDocumentversie ?documentVersions .
 					 ?document  besluitvorming:heeftVersie ?documentVersions .
-           OPTIONAL { ?document  besluitvorming:stuknummerVR ?numberVR . }
 					 ?documentVersions mu:uuid ?documentVersionId .
-           ?documentVersions ext:versieNummer ?versionNumber .
-          OPTIONAL { ?documentVersions ext:gekozenDocumentNaam ?documentVersionName }
-           ?documentVersions ext:file ?file .
-           ?download nie:dataSource ?file .
+                     ?documentVersions ext:versieNummer ?versionNumber .
+                     ?documentVersions ext:file ?file .
+                     ?download nie:dataSource ?file .
 					 ?file mu:uuid ?fileId .
 					 ?file dbpedia:fileExtension ?extension .
+                     OPTIONAL { ?document  besluitvorming:stuknummerVR ?numberVR . }
+                     OPTIONAL { ?document  dct:title ?documentTitle . }
+                     OPTIONAL { ?documentVersions ext:gekozenDocumentNaam ?documentVersionName }
 				 }
         } 
-    } GROUP BY ?documentVersionId ?numberVR ?extension ?download ?agendaitemPrio ?agendaitem_id ?documentVersionName`;
-  const data = await query(queryString);
-  return parseSparqlResults(data);
+    } GROUP BY ?documentVersionId ?numberVR ?extension ?download ?agendaitemPrio ?agendaitem_id ?documentVersionName ?documentTitle`;
+    const data = await query(queryString);
+    return parseSparqlResults(data);
 };
 
 const parseSparqlResults = (data) => {
-  if (!data) return;
-  const vars = data.head.vars;
-  return data.results.bindings.map((binding) => {
-    let obj = {};
-    vars.forEach((varKey) => {
-      if (binding[varKey]) {
-        obj[varKey] = binding[varKey].value;
-      }
+    if (!data) return;
+    const vars = data.head.vars;
+    return data.results.bindings.map((binding) => {
+        let obj = {};
+        vars.forEach((varKey) => {
+            if (binding[varKey]) {
+                obj[varKey] = binding[varKey].value;
+            }
+        });
+        return obj;
     });
-    return obj;
-  });
 };
 
 module.exports = { getAllAgendaItemsFromAgendaWithDocuments };
